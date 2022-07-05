bbb040dbda24fba29800b97dafa9609f36175c4a
madisonlowe <mcourteney@live.com>
2022-07-05 15:31:34 +0100

    Started adding DELETE routes for clearList function.

--- models/shoppingList.js
+++ models/shoppingList.js
@@ -17,3 +17,15 @@ export async function postListItem(listItem) {
   );
   return data.rows[0];
 }
+
+/* 
+PLAN FOR CREATING DELETE ROUTE:
+- we will need a router.delete path to send delete requests down
+- these requests will need to arrive at a model of what to do to the database
+- this model will need to clear all tasks from the database
+*/
+export async function clearList() {
+  const data = await pool.query("TRUNCATE TABLE shopping;");
+  console.log("The shopping list is now:", data.rows);
+  return data.rows;
+}
--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -1,4 +1,5 @@
 import express from "express";
+import ClearList from "../../w11d2_frontend-deployment-sim-and-mads/src/components/ClearList/index.js";
 import { getShoppingList, postListItem } from "../models/shoppingList.js";
 
 const router = express.Router();
@@ -13,6 +14,18 @@ router.post("/", async (req, res) => {
   const { listItem } = req.body;
   const result = await postListItem(listItem);
   res.status(201).json({ success: true, payload: result });
+});
+
+/* 
+PLAN FOR CREATING DELETE ROUTE:
+- we will need a router.delete path to send delete requests down
+- these requests will need to arrive at a model of what to do to the database
+- this model will need to clear all tasks from the database
+*/
+
+router.delete("/items", async (req, res) => {
+  const result = await ClearList();
+  res.json({ success: true, payload: result });
 });
 
 export default router;
